008f1312ba966eb2d9200608c7207c6b39b92905
BTPJ <1069113473@qq.com>
2018-08-14 18:02:40 +0800

    测试提交
    Signed-off-by: BTPJ <1069113473@qq.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
+/// 测试
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
