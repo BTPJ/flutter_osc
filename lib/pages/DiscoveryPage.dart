4baf32f86346cc2dac9684c07d39317b1bef8680
BTPJ <1069113473@qq.com>
2018-08-23 11:50:34 +0800

    完成我的静态页面
    Signed-off-by: BTPJ <1069113473@qq.com>

--- lib/pages/DiscoveryPage.dart
+++ lib/pages/DiscoveryPage.dart
@@ -1,9 +1,169 @@
 import 'package:flutter/material.dart';
 
 /// 发现Tab界面
+// ignore: must_be_immutable
 class DiscoveryPage extends StatelessWidget {
+  /// 完整的分隔线（开始和结尾）
+  static const String COMPLETE_LINE = "completeLine";
+
+  /// 左侧有间距的分隔线（中间）
+  static const String PADDING_LINE = "paddingLine";
+
+  /// 空白区域
+  static const String BLANK = "blank";
+
+  final imagePaths = [
+    "images/ic_discover_softwares.png",
+    "images/ic_discover_git.png",
+    "images/ic_discover_gist.png",
+    "images/ic_discover_scan.png",
+    "images/ic_discover_shake.png",
+    "images/ic_discover_nearby.png",
+    "images/ic_discover_pos.png",
+  ];
+
+  final titles = ["开源软件", "码云推荐", "代码片段", "扫一扫", "摇一摇", "码云封面人物", "线下活动"];
+
+  var listData = [];
+
+  /// 构造器
+  DiscoveryPage() {
+    initData(); // 初始化数据
+  }
+
+  /// 数据初始化
+  void initData() {
+    listData.add(COMPLETE_LINE);
+    // 开始三行
+    for (int i = 0; i < 3; i++) {
+      listData.add(new ListItem(imagePaths[i], titles[i]));
+      if (i == 2) {
+        listData.add(COMPLETE_LINE);
+      } else {
+        listData.add(PADDING_LINE);
+      }
+    }
+
+    listData.add(BLANK);
+    listData.add(COMPLETE_LINE);
+    // 中间两行
+    for (int i = 3; i < 5; i++) {
+      listData.add(new ListItem(imagePaths[i], titles[i]));
+      if (i == 4) {
+        listData.add(COMPLETE_LINE);
+      } else {
+        listData.add(PADDING_LINE);
+      }
+    }
+
+    // 最后两行
+    for (int i = 5; i < 7; i++) {
+      listData.add(new ListItem(imagePaths[i], titles[i]));
+      if (i == 6) {
+        listData.add(COMPLETE_LINE);
+      } else {
+        listData.add(PADDING_LINE);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Center(child: new Text("发现Tab界面"));
+    return new Padding(
+        padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+        child: new ListView.builder(
+            itemBuilder: (context, index) => renderRow(context, index),
+            itemCount: listData.length));
   }
+
+  /// 渲染Item
+  renderRow(BuildContext context, int index) {
+    var item = listData[index];
+    if (item is String) {
+      switch (item) {
+        case COMPLETE_LINE:
+          return new Divider(height: 1.0);
+          break;
+        case PADDING_LINE:
+          return new Padding(
+            padding: const EdgeInsets.fromLTRB(50.0, 0.0, 0.0, 0.0),
+            child: new Divider(
+              height: 1.0,
+            ),
+          );
+          break;
+        case BLANK:
+          return new Container(
+            height: 20.0,
+          );
+          break;
+      }
+    } else if (item is ListItem) {
+      var listItemContent = new Padding(
+        padding: const EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+        child: new Row(
+          children: <Widget>[
+            // 左侧图片
+            new Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 0.0, 10.0, 0.0),
+                child: new Image.asset(
+                  item.icon,
+                  width: 30.0,
+                  height: 30.0,
+                )),
+
+            // 中间title
+            new Expanded(
+                child:
+                    new Text(item.title, style: new TextStyle(fontSize: 16.0))),
+
+            // 右侧箭头
+            new Image.asset('images/ic_arrow_right.png',
+                height: 16.0, width: 16.0)
+          ],
+        ),
+      );
+      return new InkWell(
+          child: listItemContent,
+          onTap: () {
+            handleListItemClick(context, item);
+          });
+    }
+  }
+
+  /// item点击事件
+  void handleListItemClick(BuildContext context, ListItem item) {
+    String title = item.title;
+    switch (title) {
+      case "开源软件":
+        print("点击开源软件");
+        break;
+      case "码云推荐":
+        print("点击码云推荐");
+        break;
+      case "代码片段":
+        print("点击代码片段");
+        break;
+      case "扫一扫":
+        print("点击扫一扫");
+        break;
+      case "摇一摇":
+        print("点击摇一摇");
+        break;
+      case "码云封面人物":
+        print("点击码云封面人物");
+        break;
+      case "线下活动":
+        print("点击线下活动");
+        break;
+    }
+  }
+}
+
+/// Item数据
+class ListItem {
+  String icon;
+  String title;
+
+  ListItem(this.icon, this.title);
 }
--- lib/pages/MyInfoPage.dart
+++ lib/pages/MyInfoPage.dart
@@ -1,9 +1,111 @@
 import 'package:flutter/material.dart';
 
 /// 我的Tab界面
+// ignore: must_be_immutable
 class MyInfoPage extends StatelessWidget {
+  var titles = ["我的消息", "阅读记录", "我的博客", "我的问答", "我的活动", "我的团队", "邀请好友"];
+  var imagePaths = [
+    "images/ic_my_message.png",
+    "images/ic_my_blog.png",
+    "images/ic_my_blog.png",
+    "images/ic_my_question.png",
+    "images/ic_discover_pos.png",
+    "images/ic_my_team.png",
+    "images/ic_my_recommend.png"
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return new Center(child: new Text("我的Tab界面"));
+    return new ListView.builder(
+        itemBuilder: (context, i) => renderRow(i),
+        itemCount: titles.length * 2);
+  }
+
+  /// 渲染Item布局
+  renderRow(int i) {
+    if (i == 0) {
+      return new Container(
+        color: const Color(0xff63ca6c),
+        height: 200.0,
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Image.asset(
+                "images/ic_avatar_default.png",
+                width: 60.0,
+              ),
+              new Text("点击头像登录",
+                  style: new TextStyle(color: Colors.white, fontSize: 16.0))
+            ],
+          ),
+        ),
+      );
+    }
+
+    i -= 1;
+    if (i.isOdd) {
+      return new Divider(
+        height: 1.0,
+      );
+    }
+
+    i = i ~/ 2;
+    var listItemContent = new Padding(
+      padding: const EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+      child: new Row(
+        children: <Widget>[
+          new Padding(
+            padding: const EdgeInsets.fromLTRB(0.0, 0.0, 10.0, 0.0),
+            child: new Image.asset(
+              imagePaths[i],
+              width: 30.0,
+              height: 30.0,
+            ),
+          ),
+          new Expanded(
+              child: new Text(
+            titles[i],
+            style: new TextStyle(fontSize: 16.0),
+          )),
+          new Image.asset('images/ic_arrow_right.png',
+              width: 16.0, height: 16.0)
+        ],
+      ),
+    );
+
+    return new InkWell(
+      child: listItemContent,
+      onTap: () {
+        handleListItemClick(titles[i]);
+      },
+    );
+  }
+
+  /// item点击事件
+  void handleListItemClick(String title) {
+    switch (title) {
+      case "我的消息":
+        print("点击我的消息");
+        break;
+      case "阅读记录":
+        print("点击阅读记录");
+        break;
+      case "我的博客":
+        print("点击我的博客");
+        break;
+      case "我的问答":
+        print("点击我的问答");
+        break;
+      case "我的活动":
+        print("点击我的活动");
+        break;
+      case "我的团队":
+        print("我的团队");
+        break;
+      case "邀请好友":
+        print("邀请好友");
+        break;
+    }
   }
 }
